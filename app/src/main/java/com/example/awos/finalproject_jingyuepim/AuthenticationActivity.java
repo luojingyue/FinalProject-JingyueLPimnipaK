0019da24e67ea760751270e87febf2f0e2e073d0
Jingyue Luo <jl4964@tc.columbia.edugit config --global --listgit config --global user.name Jingyue Luo>
2018-04-12 15:18:54 -0400

    Kamille 0412 2nd

--- app/src/main/java/com/example/awos/finalproject_jingyuepim/AuthenticationActivity.java
+++ app/src/main/java/com/example/awos/finalproject_jingyuepim/AuthenticationActivity.java
@@ -1,7 +1,9 @@
 package com.example.awos.finalproject_jingyuepim;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AuthenticationActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class AuthenticationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentication);
+    }
+
+    public void openMapActivity(View view) {
+        Intent mapIntent = new Intent(this,MainActivity.class);
+        startActivity(mapIntent);
     }
 }
