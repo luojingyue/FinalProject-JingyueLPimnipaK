1607f2c42d57d1f969ea3302650236b2d5edb210
PimKang <pk2570@tc.columbia.edu>
2018-04-11 23:01:21 -0400

    PR and Lebanon Tabbed Activity

--- app/src/main/java/com/example/awos/finalproject_jingyuepim/LebanonActivity.java
+++ app/src/main/java/com/example/awos/finalproject_jingyuepim/LebanonActivity.java
@@ -118,7 +118,7 @@ public class LebanonActivity extends AppCompatActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_lebanon, container, false);
+            View rootView = inflater.inflate(R.layout.q1_lebanon, container, false);
             TextView textView = (TextView) rootView.findViewById(R.id.section_label);
             textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
             return rootView;
